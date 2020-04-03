7dc7893eaab4dfc05c2891a04065b3196b49879e
Theis Bazin <theis.bazin@outlook.com>
2020-04-03 15:51:14 +0200

    :children_crossing: Add minimal setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='pytorch_nsynth',
+      version='1.0',
+      url='https://github.com/SonyCSLParis/pytorch-nsynth',
+      author=['Theis Bazin', 'Kwon-Young Choi'],
+      author_email=['tbazin@github.com', 'kwon-young.choi@hotmail.fr'],
+      license='MIT',
+      packages=['pytorch_nsynth'],
+      zip_safe=False)
